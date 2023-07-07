83e9d2d3d1ac7e84b7016e4db688039473e1250b
KimMinSeok <kmsaa@naver.com>
2023-07-07 22:13:04 +0900

    Add - 프로젝트 아키택처 정립
    ###DESC
    1. 클린 아키택처
    - domain, data, presentation layer
    
    2. 탭바
    -  홈, 위시리스트 2개로 구성
    
    3. core
    - 앱 전반적으로 사용되는 요소를 다룸
    - theme, color, const 등등

---
+++ lib/src/core/tabbar/tabbar_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tourism_http/src/core/const/app_color.dart';
+import 'package:tourism_http/src/features/home/presentation/screen/home_page.dart';
+import 'package:tourism_http/src/features/wish_list/presentation/screen/wish_list_page.dart';
+
+class TabbarPage extends StatefulWidget {
+  const TabbarPage({super.key});
+
+  @override
+  State<TabbarPage> createState() => _TabbarPageState();
+}
+
+class _TabbarPageState extends State<TabbarPage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int tabbarIndex = 0;
+
+  void tabListener() {
+    setState(() => tabbarIndex = controller.index);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        TabController(length: 2, vsync: this, animationDuration: Duration.zero);
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: const [HomePage(), WishListPage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: GREY_COLOR,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) => controller.animateTo(index),
+        backgroundColor: WHITE_COLOR,
+        currentIndex: tabbarIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.house),
+            activeIcon: Icon(CupertinoIcons.house_fill),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            activeIcon: Icon(CupertinoIcons.search_circle_fill),
+            label: '찜',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/features/wish_list/presentation/screen/wish_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WishListPage extends StatelessWidget {
+  const WishListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(color: Colors.blue),
+    );
+  }
+}
