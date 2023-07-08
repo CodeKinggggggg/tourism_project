76f8d816a610d10e439dcee5c38b96d6ee65694e
KimMinSeok <kmsaa@naver.com>
2023-07-09 01:35:23 +0900

    Edit - 앱 이름, 로고, 색상 수정
    ####DESC
    1. 앱이름
    : 투어리즘 -> 투어리
    
    2. 로고
    : 한글로고에서 영어로고로 수정
    
    3.메인 색
    : 검정 -> 보라색

---
+++ lib/src/common/constants/app_color.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const Color TEXT_COLOR = Color(0xFF222222);
+
+// 메인 색
+const Color PRIMARY_COLOR = Color(0xFF8000FF);
+// 서브 색
+// const Color SECONDARY_COLOR = Color(0xFF000000);
+
+// 검은색
+const Color BLACK_COLOR = Color(0xFF000000);
+
+// 흰색
+const Color WHITE_COLOR = Color(0xFFFFFFFF);
+
+// 회색
+const Color DEEP_DARK_GREY_COLOR = Color(0xFF424242);
+const Color DARK_GREY_COLOR = Color(0xFF616161);
+const Color GREY_COLOR = Colors.grey;
+const Color DEEP_LIGHT_GREY_COLOR = Color(0xFFFAFAFA);
+const Color LIGHT_GREY_COLOR = Color(0xFFD6D6D6);
