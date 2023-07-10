eb4673ea37aa532e8dd300f1ee96e66ffc236012
KimMinSeok <kmsaa@naver.com>
2023-07-10 22:45:59 +0900

    Edit - 홈 갤러리 사진 받는 프로바이더
    ####DESC
    - autoDispose 삭제
    이유 : 한번 api로 부터 사진을 받을 때 시간이 오래걸리기 떄문이며 커뮤니티 처럼 데이터가 실시간이나 자주 추가되거나 삭제되거나 수정되는 데이터가 아니기 때문

--- lib/src/features/home/presentation/provider/gallery_list_provider.dart
+++ lib/src/features/home/presentation/provider/gallery_list_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tourism_http/src/features/home/domain/provider/gallery_provider.dart';
 
 // 갤러리 리스트 받는 프로바이더
-final galleryListProvider = FutureProvider.autoDispose((ref) async {
+final galleryListProvider = FutureProvider((ref) async {
   final repository = ref.watch(galleryRepositoryProvider);
   return repository.fetchGalleryList();
 });
